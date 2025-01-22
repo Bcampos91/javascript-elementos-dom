a00537f229cb91d37c88741442db32a4df5ae19d
Bruno Guerreiro <brunoguerreiro.campos@hotmail.com>
2025-01-22 19:30:25 -0300

    substituindo frases

--- Fokus-projeto-base/script.js
+++ Fokus-projeto-base/script.js
@@ -5,7 +5,7 @@ const curtoBt = document.querySelector ('.app__card-button--curto');
 const longoBt = document.querySelector('.app__card-button--longo');
 const temporizador = document.querySelector('.app__card-timer')
 const imagens = document.querySelector('.app__image')
-const frase = document.querySelector('.app__title')
+const titulo = document.querySelector('.app__title')
 const tempFoco = '1500'
 const tempDescansoCurto = '300'
 const TempDescansoLongo = '900'
@@ -14,17 +14,28 @@ const banner = document.querySelector('.app__image')
 focoBt.addEventListener('click',() => {
     html.setAttribute('data-contexto','foco')
     banner.setAttribute('src','imagens/foco.png')
+    frase.setAttribute('Otimize sua produtividade','bcshcbhscb')
 })
 
 curtoBt.addEventListener('click',() => {
     html.setAttribute('data-contexto','descanso-curto')
     banner.setAttribute('src','imagens/descanso-curto.png')
-})
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML =` Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`
+            
+            break;
+    
+        default:
+            break;
+    }
 
 longoBt.addEventListener('click',()=> {
     html.setAttribute('data-contexto','descanso-longo')
     banner.setAttribute('src','imagens/descanso-longo.png')
 })
+
 
 
 
